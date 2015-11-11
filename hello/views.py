fd287ab155c08d28bd7b43b8dcc9e32d01270e5a
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-10 17:07:05 -0800

    Adding plotting mechanism

--- hello/views.py
+++ hello/views.py
@@ -5,16 +5,27 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def test_matplotlib(request):
+    f = figure(figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    fracs = [15,30,45, 10]
+    explode=(0, 0.05, 0, 0)
+    pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
+    title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
 
+    canvas = FigureCanvasAgg(f)    
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+	matplotlib.pyplot.close(f)
+    return response
+
+	
 def db(request):
-
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
-
     return render(request, 'db.html', {'greetings': greetings})
 
