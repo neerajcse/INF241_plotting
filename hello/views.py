f1f0e400a6ba46dda65d2f4f92b0398ffa510f78
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-10 19:46:30 -0800

    Plotting by taking values from url

--- hello/views.py
+++ hello/views.py
@@ -12,10 +12,10 @@ def index(request):
     return render(request, 'index.html')
 
 def test_matplotlib(request):
+    values = [float(x) for x in request.GET.get('values').split(",")]
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     f = figure(figsize=(6,6))
-    radius = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-    area = [3.14159, 12.56636, 28.27431, 50.26544, 78.53975, 113.09724]
-    matplotlib.pyplot.plot(radius, area)
+    matplotlib.pyplot.plot(values, days)
     title('Weekly Overview', bbox={'facecolor':'0.8', 'pad':5})
     canvas = FigureCanvasAgg(f)    
     response = HttpResponse(content_type='image/png')
