edb0cc9db7a9ceee4d6ebd3f950228e7080f02cd
Neeraj Kumar <neerajpkumar@gmail.com>
2015-11-10 20:05:39 -0800

    Using normal bar chart

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,17 @@
-from pylab import figure, axes, pie, title
-import numpy as np
-from matplotlib.backends.backend_agg import FigureCanvasAgg
 from django.shortcuts import render
 from django.http import HttpResponse
-import matplotlib.pyplot
 
+# plotting library imports
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import matplotlib.pyplot as plt
+
+# scientific library imports
+import numpy as np
+
+# system imports
+import datetime
+
+# project imports
 from .models import Greeting
 
 # Create your views here.
@@ -12,12 +19,15 @@ def index(request):
     return render(request, 'index.html')
 
 def test_matplotlib(request):
-    values = [float(x) for x in request.GET.get('values').split(",")]
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    f = figure(figsize=(6,6))
-    matplotlib.pyplot.plot(np.array(values), np.array(days))
-    title('Weekly Overview', bbox={'facecolor':'0.8', 'pad':5})
-    canvas = FigureCanvasAgg(f)    
+    date = plt.figure()
+    x = [datetime.datetime(2010, 12, 1, 10, 0),
+         datetime.datetime(2011, 1, 4, 9, 0),
+         datetime.datetime(2011, 5, 5, 9, 0)]
+    y = [float(x) for x in request.GET.get('values').split(",")]
+    ax = plt.subplot(111)
+    ax.bar(x, y, width=10)
+    ax.xaxis_date()
+    canvas = FigureCanvasAgg(date)
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
     matplotlib.pyplot.close(f)
